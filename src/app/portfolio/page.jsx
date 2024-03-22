0bee809634edcb3aea4432df534da49ca633dde6
lbrMar <mar.lm@protonmail.com>
2024-03-21 17:55:21 -0700

    add content for portfolio page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,31 +1,101 @@
 "use client"
 
+import Link from "next/link";
 import { useRef } from "react";
-import { motion, useScroll } from "framer-motion";
-import BrainSvg from "@/components/BrainSvg/BrainSvg";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+const items = [
+  {
+    id: 1,
+    color: "from-red-300 to-blue-300",
+    title: "React Commerce",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "https://lama.dev",
+  },
+  {
+    id: 2,
+    color: "from-blue-300 to-violet-300",
+    title: "Next.js Medium Blog",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "https://lama.dev",
+  },
+  {
+    id: 3,
+    color: "from-violet-300 to-purple-300",
+    title: "Vanilla Book App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "https://lama.dev",
+  },
+  {
+    id: 4,
+    color: "from-purple-300 to-red-300",
+    title: "Spotify Music App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    link: "https://lama.dev",
+  },
+];
 
 const PortfolioPage = () => {
 
-  const scrollContainerRef = useRef(null);
-  const {scrollYProgress} = useScroll({container: scrollContainerRef});
+  const ref = useRef()
+
+  const { scrollYProgress } = useScroll({ target: ref })
+
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-80%"])
 
   return (
-    <>
-      <motion.div
-        className="h-full w-screen"
-        initial={{ y: "-200vh" }}
-        animate={{ y: "0" }}
-        transition={{ duration: 1 }}
-       >
-        <div className="h-screen w-screen">Hello World</div>
-      </motion.div>
-      <div className="h-[3000px] w-screen flex">
-        <div className="w-1/2"></div>
-        <div className="w-1/2 block sticky top-0 h-screen">
-          <BrainSvg scrollYProgress={scrollYProgress}/>
+    <motion.div
+      className="h-full w-screen"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0" }}
+      transition={{ duration: 1 }}
+     >
+      <div 
+        ref={ref}
+        className="h-[600vh] relative"
+      >
+        <div className="h-[calc(100vh-6rem)] w-screen flex items-center justify-center">
+          <h1 className="text-7xl md:text-8xl font-semibold text-center">MY WORKS</h1>
+        </div>
+        <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
+          <motion.div 
+            className="flex"
+            style={{ x }}
+          >
+            <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300" />
+            {items.map((item) => (
+              <div
+                key={item.id}
+                className={`w-screen h-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}
+              >
+                <div className="flex flex-col gap-8 text-white p-4">
+                  <h1 className="text-3xl font-bold">
+                    {item.title}
+                  </h1>
+                  <img 
+                    className="w-[200px] h-[200px] md:w-[400px] md:h-[400px] rounded"
+                    src={item.img} 
+                    alt="" 
+                  />
+                  <p className="w-1/2 text-sm">
+                    {item.desc}
+                  </p>
+                  <Link href={item.link} target="_blank">
+                    <button className="px-4 py-2 bg-slate-800 text-white rounded hover:bg-slate-700">
+                      See Demo
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </motion.div>
         </div>
       </div>
-    </>
+    </motion.div>
   )
 };
 
